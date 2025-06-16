48f8a6d5bab6cee7bb84f6eea71085df7efa6d4c
SammyHuang12 <jessie_hkimo@yahoo.com.tw>
2025-06-16 10:34:21 +0800

    course頁面加入swiper,更新vite版本,圖片調整,header頁面小調整

--- main.js
+++ main.js
@@ -1,4 +1,73 @@
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log("Hello world!");
+
+const courseHeaderSwiper = new Swiper(".course-header-swiper", {
+    // direction: 'horizontal',
+    slidesPerView: 'auto',
+    spaceBetween: 8,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+    },
+});
+
+const courseHotSwiper = new Swiper(".course-hot-swiper", {
+    // direction: 'horizontal',
+    // slidesGropu:3,
+    slidesPerView: 'auto',
+    spaceBetween: 24,
+    // loop: true,
+    // autoplay: {
+    //     delay: 3000,
+    // },
+    navigation: {
+        nextEl: ".swiper-button-next-hot",
+        prevEl: ".swiper-button-prev-hot",
+    },
+    breakpoints: {
+        375: {
+            // slidesPerView: 'auto',
+            // spaceBetween: 16,
+        },
+        // 576: {
+        //     //當螢幕寬度大於等於576
+        //     slidesPerView: 2,
+        //     // slidesPerView: 'auto',
+        // },
+        // 992: {
+        //     //當螢幕寬度大於等於992
+        //     slidesPerView: 4,
+        // },
+    },
+
+});
+
+const courseTopicsSwiper = new Swiper(".course-topics-swiper", {
+    // direction: 'horizontal',
+    slidesPerView: 'auto',
+    spaceBetween: 24,
+
+    navigation: {
+        nextEl: ".swiper-button-next-topics",
+        prevEl: ".swiper-button-prev-topics",
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+});
